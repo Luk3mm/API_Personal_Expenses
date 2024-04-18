7b636f5488f9080016da8f6413400bc0a8a4d47b
Lucas Mendes <lucas39lm@hotmail.com>
2024-04-17 22:07:47 -0300

    Create Cost Center

--- src/main/java/com/personal/expenses/domain/model/CostCenter.java
+++ src/main/java/com/personal/expenses/domain/model/CostCenter.java
@@ -1,9 +1,65 @@
 package com.personal.expenses.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "cost_center")
 public class CostCenter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "costCenterId")
+    private Long id;
+    @Column(nullable = false)
+    private String description;
+    @Column(columnDefinition = "TEXT")
+    private String observation;
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+    @ManyToMany(mappedBy = "cost_center")
+    @JsonBackReference
+    private List<Title> titles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Title> getTitles() {
+        return titles;
+    }
+
+    public void setTitles(List<Title> titles) {
+        this.titles = titles;
+    }
 }
