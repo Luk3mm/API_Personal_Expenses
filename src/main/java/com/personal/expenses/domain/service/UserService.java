bde569690f5df70ff5ab15cd7891dcdda98c7a71
Lucas Mendes <lucas39lm@hotmail.com>
2024-04-20 15:09:38 -0300

    User Request and User Response

--- src/main/java/com/personal/expenses/domain/service/UserService.java
+++ src/main/java/com/personal/expenses/domain/service/UserService.java
@@ -1,4 +1,33 @@
 package com.personal.expenses.domain.service;
 
-public class UserService implements InterfaceCrudService<> {
+import com.personal.expenses.dto.user.UserRequestDto;
+import com.personal.expenses.dto.user.UserResponseDto;
+
+import java.util.List;
+
+public class UserService implements InterfaceCrudService<UserRequestDto, UserResponseDto> {
+    @Override
+    public List<UserResponseDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto register(UserRequestDto dto) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto update(Long id, UserRequestDto dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
---
+++ src/main/java/com/personal/expenses/dto/user/UserResponseDto.java
@@ -0,0 +1,51 @@
+package com.personal.expenses.dto.user;
+
+import java.util.Date;
+
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String photo;
+    private Date inactivationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Date getInactivationDate() {
+        return inactivationDate;
+    }
+
+    public void setInactivationDate(Date inactivationDate) {
+        this.inactivationDate = inactivationDate;
+    }
+}
